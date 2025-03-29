a62786315ef322c03a7aeeea5d857c276a68c395
Kwon Deog Young <idejrdud@gmail.com>
2025-03-29 15:49:59 +0900

    fix: user_info 데이터가 지워지면 user_role 도 지워지도록 수정

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -23,5 +23,5 @@ CREATE TABLE IF NOT EXISTS user_role
     create_date_time TIMESTAMP(6),
     update_date_time TIMESTAMP(6),
     CONSTRAINT FK_role_id FOREIGN KEY (role_id) REFERENCES role_info (role_id),
-    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES user_info (user_id)
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES user_info (user_id) ON DELETE CASCADE
 );
