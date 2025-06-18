0cc62e49c4862a551aea929927a813c665543639
Rudy <>
2025-06-18 23:48:29 +0900

    Revert "feat: RSA key 생성 유틸 클래스 추가"
    This reverts commit 596e5a4f7421c7399b179e077aae551b285fc40f.

--- src/test/resources/sql/prod/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO role_info (role_name, create_date_time, update_date_time )
-VALUES ('ADMIN', NOW(), NOW()),
-       ('USER', NOW(), NOW()),
-       ('GUEST', NOW(), NOW())
-ON CONFLICT (role_name) DO NOTHING;
